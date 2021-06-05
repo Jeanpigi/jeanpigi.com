e9e052e794454476add0d06ce7f7749e4a705717
Jean Pierre <stick-master666@hotmail.com>
2021-06-05 10:22:22 -0500

    [add] card styling in projects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
     compress: true,
+    future: {
+        webpack5: true,
+    },
 }
--- pages/projects.js
+++ pages/projects.js
@@ -24,18 +24,21 @@ const projects = () => {
                 <title>Proyectos</title>
             </Head>
             <div className={styles.Projects}>
-                <ul>
+                <ul className={styles.Projects_card_content}>
                     {proyectos.map(({ title, description, image, url }, index) => (
-                        <li key={index}>
-                            <Link href={url}>
-                                <a className={styles.Projects_link} target="_blank" rel="noopener">
-                                    <h2> {title} </h2>
-                                    <span> {description} </span>
-                                    <picture className={styles.Projects_image}>
-                                        <img src={image} alt={title} width={250} height={150} />
-                                    </picture>
-                                </a>
-                            </Link>
+                        <li key={index} className={styles.Projects_card}>
+                            <h2 className={styles.Projects_card_title}> {title} </h2>
+                            <span className={styles.Projects_card_body}> {description} </span>
+                            <picture className={styles.Projects_image}>
+                                <img src={image} alt={title} width={250} height={150} />
+                            </picture>
+                            <div className={styles.Projects_link}>
+                                <Link href={url}>
+                                    <a className={styles.Projects_link_button} target="_blank" rel="noopener">
+                                        Visita el sitio
+                                    </a>
+                                </Link>
+                            </div>
                         </li>
                     ))}
                 </ul>
